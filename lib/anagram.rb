67cdef68c51a12f3d0a65bc9d776e0929bd75ef0
Michael Fahim <michael_fahim@yahoo.com>
2020-01-10 00:45:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,19 +1,19 @@
-# Your code goes here!
 class Anagram
-attr_accessor :word,
-def initialize(word)
-    self.word = word
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
   end
 
-	def match(words)
-	 matches = []
-	 words.each do |word|
-		 original_word = self.word.split("").sort!
-		 comparison = word.split("").sort!
-		 if original_word == comparison
-			 matches << word
-		 end
-	 end
-	 matches
- end
-end 
+  def match(array_of_words)
+     element
+
+
+    array_of_words.select do |element|
+      (@word.split("").sort) == (element.split("").sort)
+    end
+  end
+
+
+end
